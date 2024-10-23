fc8c37d7a6aa913d1614137f39ba8af8f5d5d7f1
Vasily Lavrentyev <basillavr@gmail.com>
2024-10-23 20:31:33 +0300

    hw_2

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+from lecture_2.hw.shop_api.store.db import StoreDB
+from lecture_2.hw.shop_api.routes import router_cart, router_item
+from lecture_2.hw.shop_api.store.data import ShopAPIDataSource
 
-app = FastAPI(title="Shop API")
+app = FastAPI(title='Shop API')
+app.include_router(router_cart)
+app.include_router(router_item)
+
+data = ShopAPIDataSource()
